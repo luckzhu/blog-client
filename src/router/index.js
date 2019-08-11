c4e788fd3bf6188e459b821491557a7f250f34d2
luckzhu <luckzhu1992@gmail.com>
2019-08-11 16:47:46 +0800

    优化路由，分模块引入，异步加载各模块js

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from "../components/Index.vue"
-import Login from "../components/Login.vue"
-import Register from "../components/Register.vue"
-import Create from "../components/create.vue"
-import Edit from "../components/Edit.vue"
-import My from "../components/My.vue"
-import User from "../components/User.vue"
-import Detail from "../components/Detail.vue"
+
+// import Index from "@/components/Index.vue"
+// import Login from "@/components/Login.vue"
+// import Register from "@/components/Register.vue"
+// import Create from "@/components/create.vue"
+// import Edit from "@/components/Edit.vue"
+// import My from "@/components/My.vue"
+// import User from "@/components/User.vue"
+// import Detail from "@/components/Detail.vue"
 
 Vue.use(Router)
 
 const router = new Router({
     routes: [{
         path: '/',
-        component: Index
+        component: () => import("@/components/Index.vue")
     },
     {
         path: '/login',
-        component: Login
+        component: () => import("@/components/Login.vue")
+
     },
     {
         path: '/register',
-        component: Register
+        component: () => import("@/components/Register.vue")
     },
     {
         path: '/create',
-        component: Create,
+        component: () => import("@/components/Create.vue"),
         meta: { requiresAuth: true }
     },
     {
         path: '/edit/:blogId',
-        component: Edit,
+        component: () => import("@/components/Edit.vue"),
         meta: { requiresAuth: true }
     },
     {
         path: '/my',
-        component: My,
+        component: () => import("@/components/My.vue"),
         meta: { requiresAuth: true }
     },
     {
         path: '/user/:userId',
-        component: User
+        component: () => import("@/components/User.vue")
     },
     {
         path: '/detail/:blogId',
-        component: Detail
+        component: () => import("@/components/Detail.vue")
     }
     ]
 })
@@ -56,7 +58,7 @@ router.beforeEach((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
         // this route requires auth, check if logged in
         // if not, redirect to login page.
-        store.dispatch('checkLogin').then(isLogin=>{
+        store.dispatch('checkLogin').then(isLogin => {
             if (!isLogin) {
                 next({
                     path: '/login',
@@ -66,7 +68,7 @@ router.beforeEach((to, from, next) => {
                 next()
             }
         })
-       
+
     } else {
         next() // 确保一定要调用 next()
     }
