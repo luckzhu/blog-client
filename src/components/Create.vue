c39d3da8b76988f627018b88c5cbda0344d8d68f
luckzhu <luckzhu1992@gmail.com>
2019-08-16 15:12:16 +0800

    完成edit页面功能

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="edit">
+    <div id="create">
       <h1>创建文章</h1>
       <h3>文章标题</h3>
       <el-input v-model="title"></el-input>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -3,24 +3,79 @@
     <div id="edit">
       <h1>编辑文章</h1>
       <h3>文章标题</h3>
-      <el-input></el-input>
+      <el-input v-model="title"></el-input>
       <p class="msg">限30个字</p>
       <h3>内容简介</h3>
-      <el-input type="textarea" rows="3"></el-input>
+      <el-input
+        v-model="description"
+        type="textarea"
+        :autosize="{ minRows: 4, maxRows: 6 }"
+      ></el-input>
       <p class="msg">限30个字</p>
       <h3>文章内容</h3>
-      <el-input type="textarea" rows="20"></el-input>
+      <el-input
+        v-model="content"
+        type="textarea"
+        :autosize="{ minRows: 20, maxRows: 40 }"
+      ></el-input>
       <p class="msg">限30个字</p>
-      <el-button>确定</el-button>
+
+      <p class="isAtIndex">
+        <el-switch
+          v-model="atIndex"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+        <label>在首页展示</label>
+      </p>
+      <el-button type="primary" @click="onUpdateBlog">更新</el-button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import blog from "@/api/blog.js";
+export default {
+  data() {
+    return {
+      atIndex: true,
+      title: "",
+      description: "",
+      content: ""
+    };
+  },
+  created() {
+    this.blogId = this.$route.params.blogId;
+    blog.getDetail({ blogId: this.blogId }).then(res => {
+      this.title = res.data.title;
+      this.description = res.data.description;
+      this.content = res.data.content;
+      this.atIndex = res.data.atIndex;
+    });
+  },
+  methods: {
+    onUpdateBlog() {
+      blog
+        .updateBlog(
+          { blogId: this.blogId },
+          {
+            title: this.title,
+            content: this.content,
+            description: this.description,
+            atIndex: this.atIndex
+          }
+        )
+        .then(res => {
+          this.$message.success(res.msg);
+          this.$router.push({ path: `/detail/${res.data.id}` });
+        });
+    }
+  }
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../assets/base.scss";
 
 #edit,
@@ -29,6 +84,19 @@ export default {};
 
   h1 {
     text-align: center;
+  }
+  h3 {
+    margin-bottom: 5px;
+    font-weight: 500;
+  }
+
+  .isAtIndex {
+    margin-bottom: 14px;
+    display: flex;
+    align-items: center;
+    > label {
+      margin-left: 5px;
+    }
   }
 
   .msg {
